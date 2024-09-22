2847303c476c8e6beb7513142f52da3b723e6324
Sayma <saymalubna85@gmail.com>
2024-09-22 19:55:20 +0600

    seeding test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //echo "Seeding data";
+        \App\Models\Category::factory(10)->create();
+
     }
 }
